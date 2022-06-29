d626f3c8e5239960826c643a07bc550620eb0c48
RV411 <ivan_rodriguez3000@hotmail.com>
2022-06-28 20:49:11 -0500

    frontend

---
+++ employeemanagerapp/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ServicesService } from './services.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [ServicesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
